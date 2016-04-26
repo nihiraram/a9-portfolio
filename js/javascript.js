1fc25a1527a82313471311ebf1223ebdf59d136f
Nihira Ram <nihira@Nihiras-MacBook-Pro.local>
2016-04-25 20:28:09 -0400

    commiting

---
+++ js/javascript.js
@@ -0,0 +1,23 @@
+$("#purple").click(function() {
+	document.bgColor="purple";
+});
+
+$("#orange").click(function() {
+	document.bgColor="orange";
+});
+
+$("#lgreen").click(function() {
+	document.bgColor="lightgreen";
+});
+
+$("#grey").click(function() {
+	document.bgColor="grey";
+});
+
+$("#teal").click(function() {
+	document.bgColor="teal";
+});
+
+$("#brown").click(function() {
+	document.bgColor="brown";
+});
